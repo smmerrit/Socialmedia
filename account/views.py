904d394bc4ef0d8eb4d48e9a2a6743a5be33ddef
smmerrit <smerritt98@gmail.com>
2020-09-08 16:27:47 -0700

    Tempalets and static files added some logic for goin still need to do some work on the base.html file

--- account/views.py
+++ account/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
 
 def user_login(request):
     if request.method == 'POST':
