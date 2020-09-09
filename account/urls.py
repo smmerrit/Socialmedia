b7f7140406001f20ddc3898466a085b033500426
smmerrit <smerritt98@gmail.com>
2020-09-09 06:28:02 -0700

    Setting up for password and registration, some fixes to dashboard

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,13 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard')
+    path('', views.dashboard, name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done', auth_views.PasswordResetDoneView, name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirmation'),
+    path('reset/done', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_complete')
+
 ]
