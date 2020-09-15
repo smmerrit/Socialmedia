4dc531ef1a7dd693f3723cc5d38c86451ccde9dd
smmerrit <smerritt98@gmail.com>
2020-09-15 15:30:01 -0700

    Added edit profile, and made some modifications to admin and dash

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User 
 from django.shortcuts import render
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -21,5 +22,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password1'] != cd['password2']:
             raise forms.ValidationError('non matching passwords')
         return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
 
 
--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-#from .models import Profile
+from .models import Profile
+from django.contrib import messages
+from .models import Profile
 
 @login_required
 def dashboard(request):
@@ -47,7 +49,7 @@ def register(request):
             # Save the User object
             new_user.save()
             # Create the user profile
-         #   Profile.objects.create(user=new_user)
+            #Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -56,3 +58,19 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,'Profile Updated')
+        else:
+            messages.error(request,'Error updating your profile')
+    else: 
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'account/edit.html',{'user_form': user_form, 'profile_form':profile_form})
